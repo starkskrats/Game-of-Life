8e061fcbeefd861510fa8de5ecf114ee86a9c4ef
Briana Frampton <brianaf95@yahoo.com>
2020-02-20 19:28:44 -0700

    removed razer pages

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var c = document.getElementById("gameBoard");
+var ctx = c.getContext("2d");
+for (var i = 0; i < 20; i++) {
+    var x = 30 * i;
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, 500);
+    ctx.stroke();
+}
+for (var i = 0; i < 20; i++) {
+    var x = 25 * i;
+    ctx.moveTo(0, x);
+    ctx.lineTo(600, x);
+    ctx.stroke();
+}
