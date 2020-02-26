ac79249e8e598cd3a6aa468f09ddaa2128bb86f9
Briana Frampton <brianaf95@yahoo.com>
2020-02-25 19:59:03 -0700

    Added Cells class

---
+++ Cells.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_of_Life
+{
+    public class Cells
+    {
+
+        private bool isAlive;
+        private int neighbors;
+        // rgb
+
+
+        public bool LivingStatus
+        {
+            get { return isAlive; }
+            set { isAlive = value; }
+        }
+            
+        public int NeighborCount
+        {
+            get { return neighbors; }
+            set { neighbors = value; }
+        }
+
+        // get/set for rgb
+
+        public void IncreaseNeighbors()
+        {
+            neighbors++;
+        }
+
+
+    }
+}
